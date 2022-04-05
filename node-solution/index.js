c87accfc55b398509ed1c9ef1984795b79eba24e
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2022-04-05 20:37:50 +0700

    Update solution nodejs serve

--- node-solution/index.js
+++ node-solution/index.js
@@ -2,11 +2,12 @@ const handler = require('serve-handler');
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-  // You pass two more arguments for config and middleware
-  // More details here: https://github.com/vercel/serve-handler#options
-  return handler(request, response);
+  let param = {
+    public : "../simple-webgl-build/"
+  }
+  return handler(request, response, param);
 })
 
-server.listen(3000, () => {
-  console.log('Running at http://localhost:3000');
+server.listen(3001, () => {
+  console.log('Running at http://localhost:3001');
 });
